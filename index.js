422fa6d977e9b5ac7c854a1b8f7577d346dba03f
torshow <slywoods12@gmail.com>
2026-04-15 12:22:17 +0300

    combineUsers

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 
 
+const combineUsers = (...arrays) => {
+  const users = arrays.flat();
+  const merge_date = Date.today().toString("M/d/yyyy");
+  return { users, merge_date };
+};
+
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
